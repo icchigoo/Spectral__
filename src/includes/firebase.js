403eba1c59b4f127838a783e16dbb9206d6f855b
icchigoo <icchigoo12@gmail.com>
2023-03-29 21:42:35 +0545

    firebase

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,14 +4,17 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 // Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBDl5a07lRzDowpOzL7eVEyperaQFUQC2U',
-  authDomain: 'music-5b4a5.firebaseapp.com',
-  projectId: 'music-5b4a5',
-  storageBucket: 'music-5b4a5.appspot.com',
-  messagingSenderId: '1085678914019',
-  appId: '1:1085678914019:web:2b35218aea58700553f8d6'
-}
+  apiKey: "AIzaSyDzq0kS69gK7Qmaon0vi9GInWWegsPsdJE",
+  authDomain: "spectracl-a72c0.firebaseapp.com",
+  projectId: "spectracl-a72c0",
+  storageBucket: "spectracl-a72c0.appspot.com",
+  messagingSenderId: "96891023405",
+  appId: "1:96891023405:web:d9ef76a07acbc32fec48a4",
+  
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
